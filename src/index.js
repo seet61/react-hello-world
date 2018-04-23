8776b3170709120d91b84ef01aadc8908e566434
seet61 <seet_seet@mail.ru>
2018-04-23 16:22:34 +0300

    add button example

--- src/index.js
+++ src/index.js
@@ -115,9 +115,32 @@ class Game extends React.Component {
   }
 }
 
+class ClickButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      class: "off",
+      label: "Нажми",
+    };
+    this.press = this.press.bind(this);
+  }
+
+  press() {
+    let className = (this.state.class === "off")? "on" : "off";
+    this.setState({class: className});
+  }
+
+  render() {
+    return <button onClick={this.press} className={this.state.class}>{this.state.label}</button>
+  }
+}
+
 // ========================================
 
 ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
+  <Game />, document.getElementById('root')
+);
+
+ReactDOM.render(
+  <ClickButton />, document.getElementById('app')
 );
